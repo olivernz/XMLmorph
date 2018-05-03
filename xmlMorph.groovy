9540bd9dd7893d568efbb2daff08af8efb3e039d
Oliver <github@erlewein.org>
2018-05-03 16:15:57 +1200

    Update xmlMorph.groovy

--- xmlMorph.groovy
+++ xmlMorph.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-// xmlMorph variable XML conversion script
+// XML conversion script
 // by Oliver Erlewein
 // 26/04/2018
 
@@ -9,19 +9,22 @@ import org.yaml.snakeyaml.Yaml
 import groovy.text.*
 import groovy.xml.*
 
-// 1st input parameter
+def xmlConversionFile
+def inFile
+
 if (args.size() > 1) {
 	// XML filter class
-	def xmlConversionFile = args[0]
+	xmlConversionFile = args[0].replaceAll(/.groovy/,"")
 
 	// XML intut file
-	def inFile = new File(args[1]).text
+	inFile = new File(args[1]).text
 } else {
-  println 'Please define arguments <conversion file> <input file> [<offset>]'
+	println 'Please define arguments <conversion file> <input file> [<offset>]'
 }
 
 // Date offset if needed otherwise assumes current date
 def dateOffset = (args.size() > 2) ? args[2].toInteger() : 0 
+
 
 def xmlIn = new XmlParser().parseText(inFile)
 
